74fe3c94bb4ef045e42ce9cf065d9b602c42555b
Celal Binici <celalbinici150@gmail.com>
2024-06-21 11:08:45 +0300

    Update strategy method.

--- Behavioral Design Patterns/StrategyDesignPattern.cs
+++ Behavioral Design Patterns/StrategyDesignPattern.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class StrategyDesignPattern : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public Enemy enemy;
     void Start()
     {
-        
+        enemy.setAttackStrategy(new MeeleAttackStrategy());
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            enemy.setAttackStrategy(new MeeleAttackStrategy());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            enemy.setAttackStrategy(new RangedAttackStrategy());
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            enemy.PerformAttack();
+        }
+    }
+}
+
+public interface IAttackStrategy
+{
+    void Attack(GameObject enemy);
+}
+
+public class MeeleAttackStrategy : IAttackStrategy
+{
+    public void Attack(GameObject enemy)
+    {
+        //melee codes and animations
+    }
+}
+
+public class RangedAttackStrategy : IAttackStrategy
+{
+    public void Attack(GameObject enemy)
+    {
+        //range attack codes and animations
+    }
+}
+
+public class Enemy : MonoBehaviour
+{
+
+    private IAttackStrategy m_AttackStrategy;
+    private GameObject _enemy;
+    public void setAttackStrategy(IAttackStrategy strategy)
+    {
+       m_AttackStrategy= strategy;
+    }
+
+    public void PerformAttack()
+    {
+        m_AttackStrategy.Attack(_enemy);
     }
 }
